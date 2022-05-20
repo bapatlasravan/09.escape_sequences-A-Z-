c0c9a41a73088f56bc6e795fc1fa91f1e8a3b6e3
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-05-20 19:54:50 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,47 @@
+"""
+Date: 20thth may 2022 friday
+Name: Bapatla sravan
+File Desc: escape sequences
+"""
+# print("hello\sravan")      #hello\sravan
+# print("hello\\sravan")     #hello\sravan
+# print("hello\'sravan")     #hello\sravan
+# print("hello\"sravan")     #hello\sravan
+# print("hello\asravan")     #hello\sravan
+# print("hello\bsravan")     #hellsravan
+# print("hello\csravan")     #hello\csravan
+# print("hello\dsravan")     #hello\dsravan
+# print("hello\esravan")     #hello\esravan
+# print("hello\fsravan")     #hello♀sravan """"based upo the update it wiil aper male/female symbol or new line """"
+# print("hello\gsravan")     #hello\gsravan
+# print("hello\hsravan")     #hello\hsravan
+# print("hello\isravan")     #hello\isravan
+# print("hello\jsravan")     #hello\jsravan
+# print("hello\ksravan")     #hello\ksravan
+# print("hello\lsravan")     #hello\lsravan
+# print("hello\msravan")     #hello\msravan
+# print("hello\nsravan")     #hello
+                             #sravan
+# print("hello\osravan")     #hello\osravan
+# print("hello\psravan")     #hello\psravan
+# print("hello\qsravan")     #hello\qsravan
+# print("hello\rsravan")     #sravan        """ In \r if the left side of the \ are smalle than then the right side of the slash was print"""
+# print("abcdefghi\rsravan") #sravanghi     """ In \r if the left side of the \ are big than then the left side was replace with the right side and continue the left"""
+# print("abcdef\rsravan")    #sravan        """ if both sides are same than the right of the slash will print """
+# print("hello\ssravan")     #hello\ssravan
+# print("hello\tsravan")     #hello   sravan
+# print("hello\usravan")     #\u or unicode works with only emojies
+# print("hello\vsravan")     #hello♂sravan  """"based upo the update it wiil aper male/female symbol or new line """"
+# print("hello\wsravan")     #hello\wsravan
+# print("hello\xsravan")     #\x or unicode works with only emojies
+# print("hello\ysravan")     #hello\ysravan
+# print("hello\zsravan")     #hello\zsravan
+"""unicodes"""
+
+# print('\U0001F601')          #😁
+# print('\U0001F622')          #😢
+# print('\U0001F61C')          #😝
+# print('\U0001F92A')          #🤑
+# print('\U0001F61D')          #😑
+# print('\U0001F911')          #😶
+
